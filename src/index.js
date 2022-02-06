4c032474c72c109c77519f254c1868b841400ceb
Nastya Kladova <nstsologub@gmail.com>
2022-02-06 16:18:54 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let arr = str.split("");
+
+  for (let i = 0; i < arr.length; i++) {
+    stack.push(arr[i]);
+
+    bracketsConfig.forEach((el) => {
+        if (stack[stack.length - 2] === el[0] && stack[stack.length - 1] === el[1]) {
+          stack.pop();
+          stack.pop();
+        }
+      });
+  }
+  return stack.length === 0;
+};
